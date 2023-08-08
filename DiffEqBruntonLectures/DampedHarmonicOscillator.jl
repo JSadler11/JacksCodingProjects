a0c293efdda1fd1a3e041a6807812babcdcd266a
Jack Sadler <jacksadler1592@yahoo.com>
2023-08-08 10:13:23 -0500

    Update on Osc

--- DiffEqBruntonLectures/DampedHarmonicOscillator.jl
+++ DiffEqBruntonLectures/DampedHarmonicOscillator.jl
@@ -1,5 +1,6 @@
 using LinearAlgebra
 using Plots
+using DifferentialEquations
 
 ω = 2*π #natural frequency
 d = 0.25 #damping ratio
@@ -26,4 +27,45 @@ for k in 1:n
 end
 
 plot(tF, xF[1, :], color = "black", legend = false, xlabel = "Time", ylabel = "xF[1, :]", title = "Plot of xF[1, :] vstF")
+xlabel! = "Time"
+ylabel! = "xF[1, :]"
+
+#Define differential equation
+
+function odefunc!(dx, x, p, t)
+    dx .= A * x
+end
+
+#Create the time span
+tspan = (0.0, T)
+
+#Solve the ODE using the 'ode45' function
+prob = ODEProblem(odefunc!, x0, tspan)
+sol = solve(prob, saveat = dt)
+
+#Extract the solution
+t = sol.t
+xGood = sol.u
+
+plot_layout = @layout [a b]
+
+p1 = plot(t, xF[1, :], color = "black", label = "Forward Euler")
+plot!(t, xGood[1, :], color = "red", label = "ODE 45 (RK4)")
+xlabel!("Time")
+ylabel!("Position")
+legend!()
+
+plot(p1, layout = plot_layout)
+
+
+#compute better integral using built-in MATLAB code
+#4th-order Runge Kutta
+#[TOUT, YOUT] = ODE45(ODEFUN, TSPAN, Y0)
+
+[t, xGood] = ode45( @(t, x) A*x, 0:dt:T, x0);
+
+#using function handle to define @(t, x) A*x... right hand side
+#of \dot{x} = Ax... ode45 wants us to include time in @(t, x), in 
+#case there is time dependence
+
 
